dccabf174cce87b080cf5c2a3ec00a77e9b5dac8
Zack Elliott <hubridnoxx@gmail.com>
2016-10-05 17:59:39 -0400

    Changes to grab abstracts script

--- medline_scripts/grab_abstracts_by_topic.py
+++ medline_scripts/grab_abstracts_by_topic.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 #$ -S /usr/bin/python
 
-# This script examines either a single file or a dir of files
-# and counts (1) the number of medline citations (i.e. abstracts)
-# and (2) the number of citations with mesh headings specified.
+'''
+This script examines a file or a list of files, and prints all
+of the abstracts to a file.
+'''
 
 import xml.etree.ElementTree as et
 import os
@@ -11,38 +12,45 @@ import operator
 import copy
 import sys
 
-folderPath = '/nlp/data/corpora/medline_data/xml_files'
-# folderPath = '/home1/z/zelliott/developer/cis400/test_xml'
-# folderPath = '/Users/Zack/Developer/maple/medline_scripts/test_xml/'
+def processFile(filePath, topic):
 
-if len(sys.argv) != 2:
-  print "Specify a topic: `python grab_abstracts_by_topic.py [your topic]`"
-  sys.exit()
-
-count = 0
-topic = sys.argv[1]
-
-def processFile(filename):
-  global count
-  global topic
-
-  tree = et.parse(folderPath + '/' + filename)
+  tree = et.parse(filePath)
   root = tree.getroot()
 
-  # Count each abstract/citation
+  # For each abstract/citation...
   for citation in root.iter('MedlineCitation'):
 
     # If this article has a mesh list...
     for descriptor in citation.findall('.//DescriptorName'):
+
+      # And there is a topic match...
       if descriptor.text == topic:
-        count += 1
-        print et.tostring(citation, encoding='utf8', method='xml')
+
+        # Grab each abstract
+        for abstractText in citation.findall('.//AbstractText'):
+          print '<Abstract>'
+          print abstractText.text
+          print '</Abstract>'
+
         break
 
-print topic
+def main():
 
-# for f in os.listdir(folderPath):
-processFile('medline15n0778.xml')
-# processFile('test_grab_abstracts_by_topic.xml')
+  if len(sys.argv) != 3:
+    print 'Invalid arguments'
+    print 'Usage: `python grab_abstracts_by_topic.py [file1 file2] [your_topic]`'
+    sys.exit()
 
-print count
+  files = sys.argv[1].split()
+  topic = sys.argv[2]
+
+  print '<Abstracts>'
+  print '<Topic>' + topic + '</Topic>'
+
+  for f in files:
+    processFile(f, topic)
+
+  print '</Abstracts>'
+
+if __name__ == "__main__":
+  main()
