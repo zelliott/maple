94ed690290ad744d1b5db874787a799e27531be3
Zack Elliott <hubridnoxx@gmail.com>
2016-10-13 20:44:25 -0400

    Cleaning up old xml

--- medline_scripts/plots.py
+++ medline_scripts/plots.py
@@ -4,9 +4,9 @@
 '''
 This script does the following:
 Calculates: the total number of abstracts, types and tokens
-Plots types vs. tokens (essentially a line chart, with the x-axis being 
+Plots types vs. tokens (essentially a line chart, with the x-axis being
 number of tokens processed, and the y-axis being number of types identified)
-An identical plot as the one mentioned above, but this time ignoring 
+An identical plot as the one mentioned above, but this time ignoring
 any token/type with <= 5 occurrences
 A histogram of counts of tokens (i.e. take the top 100-1000 tokens, and make a histogram of their counts)
 '''
@@ -57,16 +57,16 @@ def processFile(filePath):
   for abstractTextElement in root.findall('.//File//Abstract'):
     abstractText = abstractTextElement.text
     count += 1
-    
+
     # remove any punctuation before tokenizing.
-    abstractText = abstractText.translate(None, string.punctuation)   
+    abstractText = abstractText.translate(None, string.punctuation)
     abstractTokens = nltk.word_tokenize(abstractText)
 
-    for tok in abstractTokens:   
+    for tok in abstractTokens:
       # token count always increases
       numTokens.append(numTokens[indexTokens] + 1)
       indexTokens+=1
-      
+
       if tok in tokenDict:
         tokenDict[tok] += 1
         # if this word has occurred before, number of types stays the same
@@ -76,10 +76,10 @@ def processFile(filePath):
         # new word so number of types increases
         numTypes.append(numTypes[indexTypes] + 1)
       indexTypes += 1
-      
+
   return (tokenDict, numTokens, numTypes, count)
-  
-'''  
+
+'''
 # This methods process the file again assuming it has already been processed
 by a countDictionary. Using that, it ignores tokens and types that have appeared
 less than or equal to count times
@@ -96,17 +96,17 @@ def processFileIgnoring(filePath, countDict, count):
 
   for abstractTextElement in root.findall('.//File//Abstract'):
     abstractText = abstractTextElement.text
-    
+
     # remove any punctuation before tokenizing.
     abstractText = abstractText.translate(None, string.punctuation)
-    
+
     abstractTokens = nltk.word_tokenize(abstractText)
 
-    for tok in abstractTokens:   
+    for tok in abstractTokens:
       if (countDict[tok] > count) :
         numTokens.append(numTokens[indexTokens] + 1)
         indexTokens+=1
-      
+
         if tok in tokenDict:
           tokenDict[tok] += 1
           numTypes.append(numTypes[indexTypes])
@@ -114,23 +114,23 @@ def processFileIgnoring(filePath, countDict, count):
           tokenDict[tok] = 1
           numTypes.append(numTypes[indexTypes] + 1)
         indexTypes += 1
-      
+
   return (numTokens, numTypes)
-  
-  
-  
+
+
+
 
 def main():
-  #change this to actual filePath  
-  filePath = '/Users/omarpaladines/Desktop/CIS400/maple/medline_scripts/test_plot.xml'
+  #change this to actual filePath
+  filePath = '/Users/Zack/Developer/maple/medline_scripts/test_plot.xml'
   tokensX = []
   typesY = []
 
   #Process the file and calculate number of abstracts, tokens and types
-  tokenDict, numTokens, numTypes, abstracts = processFile(filePath)  
+  tokenDict, numTokens, numTypes, abstracts = processFile(filePath)
   tokens = countTokens(tokenDict, countPredicate)
   types = countTypes(tokenDict, countPredicate);
-  
+
   #1st plot
   tokensX = numTokens
   typesY = numTypes
@@ -140,10 +140,10 @@ def main():
   plt.grid(True)
   plt.savefig('types_vs_tokens.png')
   plt.clf()
-  
+
   #2nd plot
   count = 5
-  numTokens, numTypes = processFileIgnoring(filePath, tokenDict, count)  
+  numTokens, numTypes = processFileIgnoring(filePath, tokenDict, count)
   tokensX = numTokens
   typesY = numTypes
 
@@ -153,26 +153,26 @@ def main():
   plt.grid(True)
   plt.savefig('types_vs_tokens_ignoring_counts.png')
   plt.clf()
-  
+
 
   # 3rd plot
   # sort the dictionary by values(counts) in decreasing order
   sorted_dict = sorted(tokenDict.items(), key=operator.itemgetter(1), reverse=True)
-  
+
   # truncate the sorted_dictionary, can set this to 100 or 1000
   limit = 10
   truncated_dict = sorted_dict [0:limit]
-  
+
   # iterate over the top values of the dictionaries to get their counts
   barY = []
   for token in truncated_dict:
     barY.append(tokenDict[token[0]])
-  
-  index = np.arange(10) + 1 
+
+  index = np.arange(10) + 1
   bar_width = 0.60
-  opacity = 0.4  
+  opacity = 0.4
   error_config = {'ecolor': '0.3'}
-    
+
   # plot stuff
   plt.bar(index, barY, bar_width, alpha=opacity,
                  color='b',
