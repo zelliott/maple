8556acc8f55574a3f8bc52b47ba35f7d1a2f4edb
Zack Elliott <zelliottm@gmail.com>
2016-10-13 20:38:30 -0400

    Cleaned up dir structure

--- medline_scripts/grab_abstracts_by_topic.sh
+++ medline_scripts/grab_abstracts_by_topic.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find /nlp/data/corpora/medline_data/xml_files -type f | head -n ${1} | python grab_abstracts_by_topic.py ${2}
+find /nlp/data/corpora/medline_data/xml_files -type f | head -n ${1} | python ~/developer/maple/medline_scripts/grab_abstracts_by_topic.py ${2}
