51db773eca430d70e4b7cf314e7a935aa3171f48
Zack Elliott <hubridnoxx@gmail.com>
2016-10-19 22:49:12 -0400

    Started web app

---
+++ web_app/app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var path = require('path');
+
+var indexRoute = require('./routes/index');
+var testRoute = require('./routes/test');
+
+var port = process.env.PORT || 3000;
+
+// Define routes
+var app = express();
+
+// Views
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// Public
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
+
+// Setup routes
+indexRoute(app);
+testRoute(app);
+
+app.listen(port, function () {
+  console.log('App listening on port' + port);
+});
---
+++ web_app/routes/index.js
@@ -0,0 +1,11 @@
+var crypto = require('crypto');
+
+module.exports = function(app) {
+
+  app.get('/', function (req, res) {
+    res.render('index', {
+      passkey: crypto.randomBytes(256)
+    });
+  });
+
+};
