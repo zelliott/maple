83179619c7d0ee1b9fd8fb96bc6090f0207a02ac
Zack Elliott <hubridnoxx@gmail.com>
2017-02-10 10:24:23 -0500

    Changes

--- web_app/scripts/second_semester_analysis/analyze_tests.js
+++ web_app/scripts/second_semester_analysis/analyze_tests.js
@@ -6,7 +6,7 @@ var stemmer = new Snowball('English');
 
 var compareStem = function (a, b) {
 
-  if (_.isEmpty(b)) {
+  if (_.isEmpty(b) || _.isEmpty(a)) {
     return false;
   }
 
@@ -24,11 +24,26 @@ var compareStem = function (a, b) {
   return a === b;
 };
 
+var calculateAccuracy = function (answers, correct) {
+  var count = 0;
+
+  for (var i = 0; i < answers.length; i++) {
+    var ans = answers[i];
+    var cor = correct[i];
+
+    if (compareStem(ans, cor)) {
+      count++;
+    }
+  }
+
+  return count / answers.length;
+};
+
 var arrayToCsv = function (data) {
   var lineArray = [];
   data.forEach(function (infoArray, index) {
       var line = infoArray.join(",");
-      lineArray.push(index == 0 ? "data:text/csv;charset=utf-8," + line : line);
+      lineArray.push(line);
   });
 
   return csvContent = lineArray.join("\n");
@@ -38,6 +53,7 @@ var results = JSON.parse(fs.readFileSync('results.json', 'utf8'));
 var topics = {};
 
 var csvResults = [];
+var csvTopics = [];
 
 _.each(results, function (test) {
   var questions = test.questions;
@@ -45,30 +61,51 @@ _.each(results, function (test) {
   _.each(questions, function (question) {
     var answers = question.answers;
     var correct = question.correct;
-    var difficulty = question.difficulty;
+    var difficulty = Number(question.difficulty);
     var timeElapsed = question.timeElapsed;
     var questionId = question.questionId;
     var topic = question.topic;
 
-    var count = 0;
+    var accuracy = calculateAccuracy(answers, correct);
 
-    for (var i = 0; i < answers.length; i++) {
-      var ans = answers[i];
-      var cor = correct[i];
-
-      if (ans === cor) {
-        count++;
-      }
+    if (!topics[topic]) {
+      topics[topic] = {
+        accuracy: 0,
+        difficulty: 0,
+        accuracyCount: 0,
+        difficultyCount: 0
+      };
     }
 
-    var accuracy = count / answers.length;
+    // Cleaning
+    if (difficulty !== 0) {
+      topics[topic].difficulty += difficulty;
+      topics[topic].difficultyCount += 1;
+    }
+
+    topics[topic].accuracy += accuracy;
+    topics[topic].accuracyCount += 1;
 
     csvResults.push([ questionId, topic, accuracy, difficulty, timeElapsed ]);
   });
+});
 
-  var csvString = arrayToCsv(csvResults);
+_.each(topics, function (value, key) {
+  value.accuracy /= value.accuracyCount;
+  value.difficulty /= value.difficultyCount;
 
-  fs.writeFile('results.csv', csvString, function (err) {
-    if (err) return console.log(err);
-  });
+  var csvTopic = [ key, value.accuracy, value.difficulty, value.accuracyCount, value.difficultyCount ];
+
+  csvTopics.push(csvTopic);
+});
+
+var csvResultsStr = arrayToCsv(csvResults);
+var csvTopicsStr = arrayToCsv(csvTopics);
+
+fs.writeFile('results.csv', csvResultsStr, function (err) {
+  if (err) return console.log(err);
+});
+
+fs.writeFile('topics.csv', csvTopicsStr, function (err) {
+  if (err) return console.log(err);
 });
