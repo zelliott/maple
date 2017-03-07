c60d678a2221c10db1cd428c61f4532ed868f041
Zack Elliott <hubridnoxx@gmail.com>
2017-03-07 12:20:25 -0800

    worked on ext more

--- extension_2/content.js
+++ extension_2/content.js
@@ -1,1 +1,56 @@
-console.log('Test');
+var MapleAnalyzer = function () {
+};
+
+MapleAnalyzer.prototype = {
+
+  analyze: function () {
+    $('.rprt').each(function (i) {
+      var $item = $(this);
+      var $itemInfo = $item.find('.rprtnum');
+      var url = $item.find('a').attr('href');
+
+      var $mapleLoading = $(document.createElement('div'))
+        .text('...')
+        .addClass('maple-loading')
+        .appendTo($itemInfo);
+
+      $.ajax({
+        url: url,
+        async: true,
+        success: function (data) {
+          var abstractText = $(data).find('abstracttext').text();
+
+          // POST abstract text to our web app analysis api
+
+          var score = Math.floor(Math.random() * 100);
+          var scoreColorClass = (score < 33) ? 'maple-score-green' :
+                                (score < 66) ? 'maple-score-yellow' :
+                                               'maple-score-red';
+
+          var $mapleScore = $(document.createElement('div'))
+            .text(score)
+            .addClass('maple-score')
+            .addClass(scoreColorClass)
+            .appendTo($itemInfo);
+
+          $itemInfo.find('.maple-loading').hide();
+        },
+        error: function (xhr, status, error) {
+          var $mapleError = $(document.createElement('div'))
+            .text('!')
+            .addClass('maple-error')
+            .appendTo($itemInfo);
+
+          $itemInfo.find('.maple-loading').hide();
+        }
+      });
+    });
+  }
+
+};
+
+$(document).ready(function() {
+
+  var analyzer = new MapleAnalyzer();
+  analyzer.analyze();
+});
