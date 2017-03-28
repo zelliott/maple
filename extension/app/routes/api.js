6bb5430eb0b1e73b9d0fc35ca1b4e87db9cd077e
Zack Elliott <hubridnoxx@gmail.com>
2017-03-27 22:35:04 -0400

    changes

--- extension/app/routes/api.js
+++ extension/app/routes/api.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var model = require('../lang_model/model.json');
+
+function analyze(abstract) {
+  var unseen = 0;
+  var words = abstract.split('');
+
+  for (var i = 0; i < words.length; i++) {
+    var word = words[i].toLowerCase();
+
+    if (!model[word]) unseen++;
+  }
+
+  return unseen;
+}
 
 /* POST analyze */
 router.post('/analyze', function(req, res, next) {
   var abstractText = req.body.abstractText;
-  var score = Math.floor(Math.random() * 100);
+  var score = analyze(abstractText);
 
   res.json({
     score: score
